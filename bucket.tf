f95b7e712014c7ea546cf0ae1eb1287c720c2189
adigopi49 <94618109+adigopi49@users.noreply.github.com>
2025-02-06 22:57:28 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,1 +1,17 @@
 
+provider "aws" {
+  region = "ap-south-2"
+}
+resource "aws_s3_bucket" "s3"{
+  bucket = "adigopi499"
+}
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  bucket = aws_s3_bucket.s3.bucket
+  versioning_configuration {
+     status = "Enabled"
+  }
+}
+
+output "s3_bucket_name"{
+  value = aws_s3_bucket.s3.bucket
+}
